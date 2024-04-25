ab08a1386b059bac90259131236405a111505450
Breno Forti <fortibreno@gmail.com>
2024-04-24 22:34:33 -0300

    Feat: Invalidating Shelter Query after Update

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -8,6 +8,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useHookFormMask } from "use-mask-input";
 import { toast } from "sonner";
 import { updateShelter } from "../../../services/shelter/updateShelter";
+import { useQueryClient } from "@tanstack/react-query";
 
 const shelterSchema = z.object({
     name: z.string()
@@ -40,6 +41,8 @@ export function Shelter() {
 
     const registerWithMask = useHookFormMask(register)
 
+    const queryClient = useQueryClient()
+
     async function submit({ name, email, phone, whatsApp }: ShelterSchema) {
         const toastId = toast.loading('Salvando dados!')
 
@@ -50,7 +53,7 @@ export function Shelter() {
                 phone: phone.replace(/\D/g, ''), 
                 whatsApp: whatsApp.replace(/\D/g, ''),
             })
-
+            queryClient.invalidateQueries({queryKey: ['get-shelter']})
             toast.success('Dados salvos com sucesso!', {
                 id: toastId,
                 closeButton: true,
